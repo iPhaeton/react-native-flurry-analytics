7c3d2d403466c6a0d5c03baad12eafe35da451be
Ilya Bohaslauchyk <bohaslauchyk@gmail.com>
2017-12-05 13:07:44 +0300

    Fix index.d.ts: implicit any

--- index.d.ts
+++ index.d.ts
@@ -4,57 +4,57 @@ declare module 'react-native-flurry-analytics' {
 		/**
 		 * FlurryAnalytics.startSession('YOUR_API_KEY');
 		 */
-		startSession(apiKey: string);
+		startSession(apiKey: string): any;
 
 		/**
 		 * !!! method must be called prior to invoking startSession, e.g. FlurryAnalytics.setAppVersion('1.0.0');
 		 */
-		setAppVersion(version: string);
+		setAppVersion(version: string): any;
 
 		/**
 		 * !!! method must be called prior to invoking startSession, e.g. FlurryAnalytics.setDebugLogEnabled(false)
 		 */
-		setDebugLogEnabled(enabled: boolean);
+		setDebugLogEnabled(enabled: boolean): any;
 
 		/**
 		 * !!! method must be called prior to invoking startSession, e.g. FlurryAnalytics.setSessionContinueSeconds(10);
 		 */
-		setSessionContinueSeconds(seconds: number);
+		setSessionContinueSeconds(seconds: number): any;
 
 		/**
 		 * !!! method must be called prior to invoking startSession, e.g. FlurryAnalytics.setCrashReportingEnabled(true);
 		 */
-		setCrashReportingEnabled(enabled: boolean);
+		setCrashReportingEnabled(enabled: boolean): any;
 
 		/**
 		 * e.g. FlurryAnalytics.logEvent('eventName'); FlurryAnalytics.logEvent('eventName', {param: 'true'}, true);
 		 */
-		logEvent(eventName: string, params?: any, timed?: boolean);
+		logEvent(eventName: string, params?: any, timed?: boolean): any;
 
 		/**
 		 * e.g. FlurryAnalytics.endTimedEvent('eventName'); FlurryAnalytics.endTimedEvent('eventName', {param: 'true'});
 		 */
-		endTimedEvent(eventName: string, params?: any);
+		endTimedEvent(eventName: string, params?: any): any;
 
 		/**
 		 * e.g. FlurryAnalytics.logPageView();
 		 */
-		logPageView();
+		logPageView(): any;
 
 		/**
 		 * e.g. FlurryAnalytics.setUserId('userId');
 		 */
-		setUserId(userId: string);
+		setUserId(userId: string): any;
 
 		/**
 		 * e.g. FlurryAnalytics.setUserAge(28);
 		 */
-		setUserAge(age: number);
+		setUserAge(age: number): any;
 
 		/**
 		 * e.g. FlurryAnalytics.setUserGender('m');
 		 */
-		setUserGender(gender: 'm' | 'f');
+		setUserGender(gender: 'm' | 'f'): any;
 	}
 	const FlurryAnalytics: FlurryAnalyticsStatic;
 	export default FlurryAnalytics;
